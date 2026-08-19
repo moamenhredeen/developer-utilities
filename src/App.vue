4ce27374d5f2c35346a98422523ed21e483ced87
Moamen Hraden <moamen.hraden@almato.com>
2026-08-19 10:25:45 +0200

    feat: add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import './app.css'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1 class="text-5xl">You did it!</h1>
 </template>
 
 <style scoped></style>
